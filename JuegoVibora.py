f13f329ed5d082acb3b6141c095be0626477ef36
Mrjojosa <88664775+Mrjojosa@users.noreply.github.com>
2021-09-14 14:17:30 -0500

    Update JuegoVibora.py

--- JuegoVibora.py
+++ JuegoVibora.py
@@ -56,9 +56,7 @@ def inside(head):
     return -200 < head.x < 190 and -200 < head.y < 190
 
 
-def insideFood(food):
-    "Return True if food inside boundaries."
-    return -200 < food.x < 190 and -200< food.y < 190
+
 
 
 def move():
